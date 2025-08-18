924768bb95a8c26468ce4089bfd7bbd76dc4da30
Pedro Antonio <pedromila2000@gmail.com>
2025-08-17 22:41:13 -0300

    Adiciona novos componentes e atualiza layout do app
    Este commit inclui a adição de novos componentes como `CreateDocumentModal` e `DocumentContextMenu`, além de melhorias na estrutura do layout em arquivos como `MainLayout.razor` e `Home.razor`.
    
    Foram implementadas novas propriedades na classe `Document` e atualizações nos estilos CSS correspondentes. O projeto agora utiliza a biblioteca MudBlazor para componentes UI, com as devidas referências e configurações no `Program.cs` e `YokaiTE.csproj`.
    
    Além disso, o nome do aplicativo foi alterado de "YokaiTE" para "Haiku", e novos ícones foram adicionados ao repositório.

--- YokaiTE/Document.cs
+++ YokaiTE/Document.cs
@@ -5,5 +5,13 @@ public class Document
     public long Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime LastModified { get; set; }
+    public DateTime LastOpened { get; set; }
+
+    public string BackgroundColor { get; set; } = "#F2F2F2";
+    public bool IsPaged { get; set; }
+    public bool IsMarkdown { get; set; }
+
+    public string? Language { get; set; }
 }
--- YokaiTE/Program.cs
+++ YokaiTE/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using TG.Blazor.IndexedDB;
 using YokaiTE;
 
@@ -8,6 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 builder.Services.AddIndexedDB(dbStore =>
 {
