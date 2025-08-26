1d8d2ea73a10e0fe5f0920a504f7baf2951a9d77
kronyer <pedromila2000@gmail.com>
2025-08-26 16:19:15 -0300

    cancel deploy

--- docs/js/exportToPdf.js
+++
@@ -1,7 +0,0 @@
-﻿function exportPdfJs(title, content) {
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
-    doc.setFontSize(13);
-    doc.text(content, 10, 40);
-    doc.save(title + ".pdf");
-}
