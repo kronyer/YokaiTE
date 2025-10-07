a5b956d356a364c3dab257874ba410f406fb1849
kronyer <pedromila2000@gmail.com>
2025-10-07 13:52:39 -0300

    feat favorite document logic and layout, minor bugfixes

--- YokaiTE/Entities/DocumentMetadata.cs
+++ YokaiTE/Entities/DocumentMetadata.cs
@@ -8,6 +8,7 @@ public class DocumentMetadata
     public DateTime LastModified { get; set; }
     public DateTime LastOpened { get; set; }
     public string BackgroundColor { get; set; } = "#F2F2F2";
+    public bool Favorite { get; set; } = false;
     public string? PreviewPngBase64 { get; set; }
 
 }
